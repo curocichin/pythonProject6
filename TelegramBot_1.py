674808c093f87af76a27c941a57526acbd7a8272
venisa <valik14@gmail.com>
2022-07-01 12:30:19 +0300

    add Task 17.9.1

--- TelegramBot_1.py
+++ TelegramBot_1.py
@@ -1,3 +1,6 @@
+
+
+
 import telebot
 from TelegramBot_3 import keys, TOKEN
 from TelegramBot_2 import ConvertionException, CryptoConverter
@@ -15,25 +18,27 @@ def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key,))
-        bot.reply_to(message,text)
-
+    bot.reply_to(message,text)
 
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(' ')
 
-    if len(values) != 3:
-        raise ConvertionException('Слишком много параметров.')
-
-    quote, base, amount = values
-    total_base = CryptoConverter.convert(quote, base, amount)
-    # total_base.lower()
-
-    text = f'Стоимость {amount} {quote} в {base} - {total_base}'
-    bot.send_message(message.chat.id, text)
+    try:
+        values = message.text.split(' ')
 
 
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
 
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+    else:
+         text = f'Стоимость  {amount} {quote} в {base} - {total_base}'
+         bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
--- TelegramBot_2.py
+++ TelegramBot_2.py
@@ -1,38 +1,3 @@
-# import requests
-# import json
-# from TelegramBot_3 import keys
-#
-#
-# class ConvertionException(Exception):
-#     pass
-#
-# class CryptoConverter:
-#     @staticmethod
-#     def convert(quote: str, base: str, amount: str):
-#         if quote == base:
-#             raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-#
-#     try:
-#         quote_ticker = keys[quote]
-#     except KeyError:
-#         raise ConvertionException(f'Не удалось обрабдтать валюту {quote}')
-#
-#     try:
-#         base_ticker = keys[base]
-#     except  KeyError:
-#         raise ConvertionException(f'Не удалось обрабдтать валюту {base}')
-#
-#     try:
-#         amount = float(amount)
-#     except ValueError:
-#         raise ConvertionException(f'Не удалось обрабдтать количество {amount}')
-#
-#     r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker},')
-#     total_base = json.loads(r.content)[keys[base]]
-#   # return  total_base
-
-
-
 
 import json
 import requests
@@ -67,4 +32,3 @@ class CryptoConverter:
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = (json.loads(r.content)[keys[base]])*amount
         return total_base
-
